c83e68f8bf4f15e6d24db2b3b51a105314d7b190
Miroslav <mirkatabga@gmail.com>
2015-06-18 22:51:56 +0300

    Added Javascript OOP/01. FuncAndFuncExpression/01. FuncAndFuncExpression.sln

---
+++ Front-End-Track-2015/Javascript OOP/01. FuncAndFuncExpression/01. Task/Task-1.js
@@ -0,0 +1,56 @@
+﻿/*
+
+Task 1.
+
+Write a function that sums an array of numbers:
+Numbers must be always of type Number
+Returns null if the array is empty
+Throws Error if the parameter is not passed (undefined)
+Throws if any of the elements is not convertible to Number
+
+*/
+
+function solve() {
+    return function sumElementsInArray (array) {
+        var i,
+            len,
+            sum = 0;
+
+
+            if (sumElementsInArray.arguments.length === 0) {
+                throw undefined;
+            }
+
+            if (array.length === 0) {
+                return null;
+            }
+
+            else {
+                var areDigits = array.every(function (item) {
+                    return !isNaN(item);
+                })
+
+                if (areDigits) {
+                    return array.reduce(function (a,b) {
+                        return parseInt(a) + parseInt(b);
+                    })
+                }
+                else {
+                    throw 'error';
+                }
+            }
+    }
+}
+
+(function(){
+    var func = solve();
+    var test1 = [1, 2, 3];
+    var test3 = ['asass', 1, 2, 3];
+
+    //test1
+    func(test1);
+    //test2
+    func();
+    //test3;
+    func(test3);
+}());
---
+++ Front-End-Track-2015/Javascript OOP/01. FuncAndFuncExpression/02. Task/Task-2.js
@@ -0,0 +1,71 @@
+﻿/*
+Task 2.
+
+Write a function that finds all the prime numbers in a range
+It should return the prime numbers in an array
+It must throw an Error if any of the range params is not convertible to Number
+It must throw an Error if any of the range params is missing
+
+*/
+
+
+function solve() {
+    return function findPrimesInRange(start, end) {
+
+        var primes = [],
+            numbers = [],
+            i,
+            isPrime = true,
+            len;
+
+        if (findPrimesInRange.arguments.length !== 2) {
+            throw 'Error! findPrimesInRange takes two arguments';
+        }
+
+        var isConvertible = true;
+
+        for (i = 0; i < arguments.length; i++) {
+            if (isNaN(arguments[i])) {
+                isConvertible = false;
+            }
+
+        }
+
+        if (isConvertible){
+            for (i = start; i <= end; i++) {
+                numbers.push(i);
+            }
+
+            for (i = 0, len = numbers.length; i < len; i++) {
+                isPrime = true;
+                if (numbers[i] > 2) {
+                    for (var j = 2; j <= Math.sqrt(numbers[i]) ; j++) {
+                        if (!(numbers[i] % j)) {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+
+                    if (isPrime) {
+                        primes.push(numbers[i]);
+                    }
+                }
+                else if (numbers[i] === 2) {
+                    primes.push(numbers[i]);
+                }
+                
+            }
+
+            return primes;
+        }
+        else {
+            throw 'Error! findPrimesInRange arguments must be Numbers';
+        }
+    }
+}
+
+(function () {
+
+    var func = solve();
+    func(1, 5);
+}());
