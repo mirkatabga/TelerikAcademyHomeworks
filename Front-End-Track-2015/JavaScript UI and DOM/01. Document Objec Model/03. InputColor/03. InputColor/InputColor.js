5556413f491efd9c42e24bc73e4a7da15f6b0e14
Miroslav <mirkatabga@gmail.com>
2015-07-19 02:57:49 +0300

    Added solution task3 DOM

---
+++ Front-End-Track-2015/JavaScript UI and DOM/01. Document Objec Model/03. InputColor/03. InputColor/InputColor.js
@@ -0,0 +1,8 @@
+﻿(function(){
+    var colorElem = document.getElementById('color'),
+        bodyElem = document.getElementsByTagName('body')[0];
+
+    colorElem.oninput = function () {
+        bodyElem.style.backgroundColor = colorElem.value;
+    }
+}())
