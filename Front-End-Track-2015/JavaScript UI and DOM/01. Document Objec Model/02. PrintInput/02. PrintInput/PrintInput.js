b0d886fe619f41e3973fcf9c8161adc09e40796a
Miroslav <mirkatabga@gmail.com>
2015-07-19 02:41:03 +0300

    Added solution. task-2 DOM

---
+++ Front-End-Track-2015/JavaScript UI and DOM/01. Document Objec Model/02. PrintInput/02. PrintInput/PrintInput.js
@@ -0,0 +1,11 @@
+﻿//Create a function that gets the value of <input
+//type="text"> ands prints its value to the console
+
+(function () {
+    var textField = document.getElementById('input'),
+        printBtn = document.getElementById('print');
+
+    printBtn.onclick = function () {
+        console.log(textField.value);
+    }
+}())
